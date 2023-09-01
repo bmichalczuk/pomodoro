00616539f41e59608559478c608d05ffd2f570e9
bmichalczuk <barmic90@hotmail.com>
2023-09-01 20:44:27 +0200

    add cosntans for dom nodes elements, change some class names

---
+++ tsc/consts.ts
@@ -0,0 +1,14 @@
+//DOM NODES
+const WORK_SETTINGS = `.settings-work`;
+const WORK_TIME_INCREASE_BTN = `#work-time-increase`;
+const WORK_TIME_DECREASE_BTN = `#work-time-decrease`;
+const WORK_TIME_DISPLAY = `#work-time-display`;
+
+const BREAK_SETTINGS = `.settings-break`;
+const BREAK_TIME_INCREASE_BTN = `#break-time-increase`;
+const BREAK_TIME_DECREASE_BTN = `#break-time-decrease`;
+const BREAK_TIME_DISPLAY = `#break-time-display`;
+
+const IIME_LEFT_DISPLAY = `#time-left-display`;
+const TIME_LEFT_INDICATOR = `#time-left-indicator`
+const TIME_STOP_BTN = `#start-stop-btn`;
