5ac1490c5e2f50b805abd696d93027cd994c8e5d
bmichalczuk <barmic90@hotmail.com>
2023-09-01 21:12:59 +0200

    add dom nodes consts

--- tsc/consts.ts
+++ tsc/consts.ts
@@ -1,14 +1,12 @@
 //DOM NODES
-const WORK_SETTINGS = `.settings-work`;
-const WORK_TIME_INCREASE_BTN = `#work-time-increase`;
-const WORK_TIME_DECREASE_BTN = `#work-time-decrease`;
-const WORK_TIME_DISPLAY = `#work-time-display`;
+export const WORK_TIME_INCREASE_BTN = `#work-time-increase`;
+export const WORK_TIME_DECREASE_BTN = `#work-time-decrease`;
+export const WORK_TIME_DISPLAY = `#work-time-display`;
 
-const BREAK_SETTINGS = `.settings-break`;
-const BREAK_TIME_INCREASE_BTN = `#break-time-increase`;
-const BREAK_TIME_DECREASE_BTN = `#break-time-decrease`;
-const BREAK_TIME_DISPLAY = `#break-time-display`;
+export const BREAK_TIME_INCREASE_BTN = `#break-time-increase`;
+export const BREAK_TIME_DECREASE_BTN = `#break-time-decrease`;
+export const BREAK_TIME_DISPLAY = `#break-time-display`;
 
-const IIME_LEFT_DISPLAY = `#time-left-display`;
-const TIME_LEFT_INDICATOR = `#time-left-indicator`
-const TIME_STOP_BTN = `#start-stop-btn`;
+export const IIME_LEFT_DISPLAY = `#time-left-display`;
+export const TIME_LEFT_INDICATOR = `#time-left-indicator`
+export const START_STOP_BTN = `#start-stop-btn`;
