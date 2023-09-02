2d6318b07fe449671ed4607213ec0cf5282d2157
bmichalczuk <barmic90@hotmail.com>
2023-09-02 21:48:48 +0200

    add file

---
+++ tsc/dom-handlers.ts
@@ -0,0 +1,2 @@
+import { decreaseBreakTimeBtn, increaseBreakTimeBtn, decreaseWorkTimeBtn, increaseWorkTimeBtn, timeLeftDisplay, workTimeDisplay, breakTimeDisplay, startStopBtn, timeLeftIndicator } from "./dom-nodes";
+
