0f21fd84b1323be759b8f7156a74a76b52157f83
bmichalczuk <barmic90@hotmail.com>
2023-09-03 21:48:54 +0200

    add functions to handle dom changes

--- tsc/dom-handlers.ts
+++ tsc/dom-handlers.ts
@@ -1,2 +1,12 @@
-import { decreaseBreakTimeBtn, increaseBreakTimeBtn, decreaseWorkTimeBtn, increaseWorkTimeBtn, timeLeftDisplay, workTimeDisplay, breakTimeDisplay, startStopBtn, timeLeftIndicator } from "./dom-nodes";
+import {timeLeftDisplay, workTimeDisplay, breakTimeDisplay, timeLeftIndicator } from "./dom-nodes";
+
+const changeTextValue = (node: Element) => (value: number) => node.textContent = String(value);
+
+export const changeWorkTimeDisplay = changeTextValue(workTimeDisplay);
+export const changeBreakTimeDisplay = changeTextValue(breakTimeDisplay);
+export const changeCurrentTimeLeft = changeTextValue(timeLeftDisplay);
+
+export const changeTimeLeftIndicator = (height: string) => timeLeftIndicator.style.height = height;
+
+
 
