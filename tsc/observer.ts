3cedfb520056013c9e77573470d6b9154309a01e
bmichalczuk <barmic90@hotmail.com>
2023-09-03 22:37:07 +0200

    add functions for changing state and actualize events list

--- tsc/observer.ts
+++ tsc/observer.ts
@@ -1,12 +1,11 @@
-enum EVENTS_LIST {
-    increasedBreakTime = 'increaseBreakTime',
-    decreasedBreakTime = 'decreaseBreakTime',
-    increasedWorkTime = 'increaseWorkTime',
-    decreasedWorkTime = 'decreaseWorkTime',
+export enum EVENTS_LIST {
+    changedWorkTime = 'changedWorkTime',
+    changedBreakTime = 'changedBreakTime',
     startedTimer = 'startTimer',
     stopedTimer = 'stopTimer',
     changedRound = 'changedRound',
-    resetTimer = 'resetTimer'
+    resetTimer = 'resetTimer',
+    changedCurrentTimeLeft = 'changedCurrentTimeLeft'
 }
 
 type EventCallback = (State: {}) => void;
@@ -15,23 +14,21 @@ type EventsType = {[key in EVENTS_LIST]: EventsCallbackArray};
 
 const EVENTS: EventsType = {
     [EVENTS_LIST.changedRound]: [() => {console.log(1)}],
-    [EVENTS_LIST.decreasedBreakTime]: [],
-    [EVENTS_LIST.increasedBreakTime]: [],
-    [EVENTS_LIST.decreasedWorkTime]: [() => console.log(1), () => console.log(3), () => console.log(2)],
-    [EVENTS_LIST.increasedWorkTime]: [],
+    [EVENTS_LIST.changedBreakTime]: [],
+    [EVENTS_LIST.changedWorkTime]: [],
     [EVENTS_LIST.startedTimer]: [],
     [EVENTS_LIST.stopedTimer]: [],
     [EVENTS_LIST.resetTimer]: [],
+    [EVENTS_LIST.changedCurrentTimeLeft]: []
 };
 
 //fireEvent
-const fireEvent = (eventName: EVENTS_LIST) => {
+export const fireEvent = (eventName: EVENTS_LIST) => {
     if(!EVENTS[eventName] || EVENTS[eventName].length < 1) {
         return;
     }
     EVENTS[eventName].forEach(async (cb: EventCallback) =>  await cb({}));
 }
-fireEvent(EVENTS_LIST.decreasedWorkTime);
 
 //addCallback
 const addEventCallback = (eventName: EVENTS_LIST, cb: EventCallback) => {
